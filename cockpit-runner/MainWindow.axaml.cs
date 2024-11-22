06bb1aeff2131ea4d95cbd4cbeda6c51dea9783d
ztarbug <kronprinzfriedrich@gmx.de>
2024-11-22 15:16:26 +0100

    add Installer, work on Docker Desktop check

--- cockpit-runner/MainWindow.axaml.cs
+++ cockpit-runner/MainWindow.axaml.cs
@@ -11,53 +11,29 @@ namespace cockpit_runner;
 
 public partial class MainWindow : Window
 {
+    private string cockpitDir = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), ".aicockpit");
+    private bool isCockpitRunning = false;
+
     public MainWindow()
     {
         InitializeComponent();
         Height = 400;
         Width = 600;
-        StartStopCockpit.IsEnabled = false;
+        StartStopCockpitBtn.IsEnabled = false;
 
         List<string> scenarios = new List<string>();
         scenarios.Add("Traffic");
-        scenarios.Add("garbage mangement");
-        scenarios.Add("Health Care");
-        scenarios.Add("Event Management");
+        scenarios.Add("Garbage");
+        scenarios.Add("Health");
+        scenarios.Add("Event");
         SelectScenario.ItemsSource = scenarios;
     }
 
-    public async void Button_Click(object sender, RoutedEventArgs args)
+    public async void PreRequisites_Click(object sender, RoutedEventArgs args)
     {
-        PreReqOutput.Text += "Checking pre requisites... \n";
+        PreReqOutput.Text += "Checking pre-requisites... \n";
         CheckIfDockerIsInstalled();
         CheckIfCodeiIsPresent();
-    }
-
-    public async void StartStopCockpit_Click(object sender, RoutedEventArgs args)
-    {
-        //TODO docker compose up
-        var stdOutBuffer = new StringBuilder();
-        var stdErrBuffer = new StringBuilder();
-        try
-        {
-            var result = await Cli.Wrap("docker")
-                .WithArguments(["compose", "up" , "-f", "demo-docker-compose.up"])
-                .WithWorkingDirectory(".")
-                .WithValidation(CommandResultValidation.None)
-                .WithStandardOutputPipe(PipeTarget.ToStringBuilder(stdOutBuffer))
-                .WithStandardErrorPipe(PipeTarget.ToStringBuilder(stdErrBuffer))            
-                .ExecuteAsync();
-
-            var stdOut = stdOutBuffer.ToString();
-            var stdErr = stdErrBuffer.ToString();
-
-            PreReqOutput.Text += "Docker command is present \n";
-            StartStopCockpit.IsEnabled = true;
-            CheckIfCockpitIsRunning();
-        } catch(System.ComponentModel.Win32Exception e)
-        {
-            PreReqOutput.Text += "Can't detect docker, please install Docker/Docker desktop ";
-        }
     }
 
     private async void CheckIfDockerIsInstalled()
@@ -71,16 +47,61 @@ public partial class MainWindow : Window
                 .WithWorkingDirectory(".")
                 .WithValidation(CommandResultValidation.None)
                 .WithStandardOutputPipe(PipeTarget.ToStringBuilder(stdOutBuffer))
-                .WithStandardErrorPipe(PipeTarget.ToStringBuilder(stdErrBuffer))            
+                .WithStandardErrorPipe(PipeTarget.ToStringBuilder(stdErrBuffer))
                 .ExecuteAsync();
 
             var stdOut = stdOutBuffer.ToString();
             var stdErr = stdErrBuffer.ToString();
 
             PreReqOutput.Text += "Docker command is present \n";
-            StartStopCockpit.IsEnabled = true;
+            StartStopCockpitBtn.IsEnabled = true;
             CheckIfCockpitIsRunning();
-        } catch(System.ComponentModel.Win32Exception e)
+        }
+        catch (System.ComponentModel.Win32Exception e)
+        {
+            PreReqOutput.Text += "Can't detect docker, please install Docker/Docker desktop ";
+        }
+    }
+
+    private async void StartStopCockpit_Click(object sender, RoutedEventArgs args)
+    {
+        var command = "up";
+        CheckIfCockpitIsRunning();
+        if(isCockpitRunning == true)
+        {
+            command = "down";
+        }
+
+        var composeDirectory = Path.Combine(cockpitDir, "docker-compose");
+        var stdOutBuffer = new StringBuilder();
+        var stdErrBuffer = new StringBuilder();
+        try
+        {
+            var result = await Cli.Wrap("docker")
+                .WithArguments(["compose", "-f", "docker-compose.yaml", command])
+                .WithWorkingDirectory(composeDirectory)
+                .WithValidation(CommandResultValidation.None)
+                .WithStandardOutputPipe(PipeTarget.ToStringBuilder(stdOutBuffer))
+                .WithStandardErrorPipe(PipeTarget.ToStringBuilder(stdErrBuffer))
+                .ExecuteAsync();
+
+            var stdOut = stdOutBuffer.ToString();
+            var stdErr = stdErrBuffer.ToString();
+
+            ActionOutput.Text += "Starting Docker compose \n";
+            ActionOutput.Text += stdOut + "\n";
+            ActionOutput.Text += stdErr + "\n";
+            StartStopCockpitBtn.IsEnabled = true;
+            if (command == "up")
+            {
+                StartStopCockpitBtn.Content = "Stop Cockpit";
+            }
+            else
+            {
+                StartStopCockpitBtn.Content = "Start Cockpit";
+            }
+        }
+        catch (System.ComponentModel.Win32Exception e)
         {
             PreReqOutput.Text += "Can't detect docker, please install Docker/Docker desktop ";
         }
@@ -103,24 +124,25 @@ public partial class MainWindow : Window
             var stdOut = stdOutBuffer.ToString();
             var stdErr = stdErrBuffer.ToString();
 
-            if (stdOut.Contains("cockpit"))
+            ActionOutput.Text += stdOut + "\n";
+
+            if (stdOut.Contains("aic"))
             {
                 ActionOutput.Text += "Cockpit is running \n";
-                StartStopCockpit.Content = "Stop Cockpit";
+                StartStopCockpitBtn.Content = "Stop Cockpit";
+                isCockpitRunning = true;
                 return;
             }
-
             ActionOutput.Text += "Cockpit is not running \n";
         } catch(System.ComponentModel.Win32Exception e)
         {
             ActionOutput.Text += "Can't run Docker, please check if is installed ";
         }
+        isCockpitRunning = false;
     }
 
     private void CheckIfCodeiIsPresent()
     {
-        var homeDir = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
-        var cockpitDir = Path.Combine(homeDir, ".aicockpit");
         Directory.CreateDirectory(cockpitDir);
         // check if dir is empty
         if(Directory.GetFiles(cockpitDir).Length == 0 && Directory.GetDirectories(cockpitDir).Length == 0)
@@ -130,7 +152,7 @@ public partial class MainWindow : Window
 
         } else
         {
-            // TODO
+            PreReqOutput.Text += "Code appears to be alread present";
         }
     }
 }
